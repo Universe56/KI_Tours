ac52a8842ecc08c3a3915d35063e9f5f20db8e12
Арсен <haarar93@gmail.com>
2023-09-18 13:16:17 +0400

    first commit

--- language-changer.js
+++ language-changer.js
@@ -31,3 +31,32 @@ languageLinks.forEach(link => {
     link.addEventListener('click', handleLanguageSelection);
 });
 
+
+
+function loadContent() {
+    const mainElement = document.querySelector('main');
+
+    // Define content for different languages
+    const content = {
+        en: {
+            about: "English content for About section...",
+            tours: "English content for Tours section...",
+            contacts: "English content for Contacts section..."
+        },
+        ru: {
+            about: "Russian content for About section...",
+            tours: "Russian content for Tours section...",
+            contacts: "Russian content for Contacts section..."
+        }
+    };
+
+    // Get the content based on the selected language
+    const selectedContent = content[language];
+
+    // Update the content of the mainElement
+    mainElement.innerHTML = `
+        <div class="about">${selectedContent.about}</div>
+        <div class="tours">${selectedContent.tours}</div>
+        <div class="contacts">${selectedContent.contacts}</div>
+    `;
+}
